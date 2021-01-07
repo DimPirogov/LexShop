7c66298143c86333c704addd19388a4d9e064674
Dimitri Pirogov <dimpirogovp@hotmail.com>
2021-01-07 23:25:44 +0100

    Product Manager Pages Build and Test

--- LexShop/LexShop.Web.UI/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LexShop.Web.UI.MvcApplication" Language="C#" %>
--- LexShop/LexShop.WebUI/Properties/AssemblyInfo.cs
+++ LexShop/LexShop.WebUI/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2e84e21b-841f-4a81-8248-593b794ab06a")]
+[assembly: Guid("6133b544-8887-48ed-a4e1-94dbe4d17f03")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
