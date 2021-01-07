866fc14c14140dd89480b98c885a8e1384aea5dc
Dimitri Pirogov <dimpirogovp@hotmail.com>
2021-01-07 16:14:01 +0100

    Initial Solution Setup

---
+++ LexShop/LexShop.Web.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LexShop.Web.UI.Startup))]
+namespace LexShop.Web.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
