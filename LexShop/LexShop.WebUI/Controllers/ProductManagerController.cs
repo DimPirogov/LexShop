6462e94df14d1981a5252ad4446163b0337a57dd
Dimitri Pirogov <dimpirogovp@hotmail.com>
2021-01-11 18:37:06 +0100

    Product Grouping adding

--- LexShop/LexShop.WebUI/Controllers/ProductManagerController.cs
+++ LexShop/LexShop.WebUI/Controllers/ProductManagerController.cs
@@ -5,15 +5,19 @@ using System.Web;
 using System.Web.Mvc;
 using LexShop.Core.Models;
 using LexShop.DataAccess.InMemory;
+using LexShop.Core.ViewModels;
 
 namespace LexShop.WebUI.Controllers
 {
     public class ProductManagerController : Controller
     {
         ProductRepository context;
+        ProductCategoryRepository productCategories;
+
         public ProductManagerController()
         {
             context = new ProductRepository();
+            productCategories = new ProductCategoryRepository();
         }
         // GET: ProductManager
         public ActionResult Index()
@@ -23,8 +27,11 @@ namespace LexShop.WebUI.Controllers
         }
         public ActionResult Create()
         {
-            Product product = new Product();
-            return View(product);
+            ProductManagerViewModel viewModel = new ProductManagerViewModel();
+            
+            viewModel.Product = new Product();
+            viewModel.ProductCategories = productCategories.Collection();
+            return View(viewModel);
         }
         [HttpPost]
         public ActionResult Create(Product product)
@@ -49,7 +56,10 @@ namespace LexShop.WebUI.Controllers
             }
             else
             {
-                return View(product);
+                ProductManagerViewModel viewModel = new ProductManagerViewModel();
+                viewModel.Product = product;
+                viewModel.ProductCategories = productCategories.Collection();
+                return View(viewModel);
             }
         }
         [HttpPost]
