44f72d33ff6eb66ca8409c46127d9b2a814793a3
Dimitri Pirogov <dimpirogovp@hotmail.com>
2021-01-30 17:48:20 +0100

    Adding new migrations files(wrongly started customer)

---
+++ LexShop/LexShop.DataAccess.SQL/Migrations/202101301608302_AfterProject.cs
@@ -0,0 +1,72 @@
+namespace LexShop.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AfterProject : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.OrderItems", "OrderId", "dbo.Orders");
+            DropIndex("dbo.OrderItems", new[] { "OrderId" });
+            DropTable("dbo.Customers");
+            DropTable("dbo.OrderItems");
+            DropTable("dbo.Orders");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.Orders",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        FirstName = c.String(),
+                        Surname = c.String(),
+                        Email = c.String(),
+                        Street = c.String(),
+                        City = c.String(),
+                        State = c.String(),
+                        ZipCode = c.String(),
+                        OrderStatus = c.String(),
+                        CreateAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.OrderItems",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        OrderId = c.String(maxLength: 128),
+                        ProductId = c.String(),
+                        ProductName = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        image = c.String(),
+                        Quantity = c.Int(nullable: false),
+                        CreateAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        UserId = c.String(),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        Email = c.String(),
+                        Street = c.String(),
+                        City = c.String(),
+                        State = c.String(),
+                        ZipCode = c.String(),
+                        CreateAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateIndex("dbo.OrderItems", "OrderId");
+            AddForeignKey("dbo.OrderItems", "OrderId", "dbo.Orders", "Id");
+        }
+    }
+}
